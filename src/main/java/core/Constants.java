b496eaaf73ae38c868f713aa8a7a16c9669ab997
Felix Wolf <x3mdroid@gmail.com>
2021-05-27 20:19:21 +0200

    change subscribed topic

--- src/main/java/core/Constants.java
+++ src/main/java/core/Constants.java
@@ -1,6 +1,8 @@
+package core;
+
 public class Constants {
 
-    final static String TOPIC = "my-example-topic";
+    final static String MailInfoTopic = "mail_server_info";
     final static String BOOTSTRAP_SERVERS = "localhost:9092";
 
 }
